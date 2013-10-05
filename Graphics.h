42a779ab400358e455cfa262a82c097cd503fc78
spikefinger <spikefinger@gmail.com>
2013-10-05 11:50:51 -0600

    Relocated ColorVertex struct
    Moved ColorVertex struct from main.cpp to increase portability (since main.cpp is only a test file).

--- Graphics.h
+++ Graphics.h
@@ -2,8 +2,12 @@
 // File name: Graphics.h
 // Author: Latisha Harris and David Emerson
 // Description: Holds material parameters and texture
-//				information for 3D surfaces, and Meshes/Camera varables and functions.
+//				information for 3D surfaces, and 
+//				Meshes/Camera varables and functions.
 //////////////////////////////////////////////////////
+
+#pragma once
+
 #include <windows.h>
 #include "settings.h"
 #include "Camera.h"
@@ -15,7 +19,10 @@
 #include MakePath(DXIncludePath, d3d9.h)
 #include MakePath(DXIncludePath, d3dx9.h)
 
-
-
-
-
+struct ColorVertex
+{
+    D3DXVECTOR3 pos;
+    D3DXVECTOR3 normal;
+    D3DCOLOR color;
+	static IDirect3DVertexDeclaration9* Decl;
+};
