6b054abe014331893865ae898f214996efa7cc4c
Latisha Harris <latishaharris1823@gmail.com>
2013-10-06 00:06:29 -0400

    New Version of Camera cpp and h. files

---
+++ Camera.h
@@ -0,0 +1,33 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "settings.h"
+#include <d3d9.h>
+#include <d3dx9.h>
+
+class Camera {
+public:
+	Camera();
+	~Camera();
+
+
+	D3DXVECTOR3		pos, look, up;
+	float		fov;
+	float		ar;
+	float		nearClip, farClip;
+
+
+	void Init();
+	void Init(D3DXVECTOR3 _pos, D3DXVECTOR3 _look, D3DXVECTOR3 _up, float _fov, float _ar, float _nearClip, float _farClip);
+	void SetAR(int wd, int ht);
+	void dxGetProjection(D3DXMATRIX* pm);
+	void dxSetProjection(LPDIRECT3DDEVICE9 pd3dDevice);
+	void dxSetView(LPDIRECT3DDEVICE9 pd3dDevice);
+	void dxGetView(D3DXMATRIX* pm);
+	void MoveForward(float distance);
+	void MoveRight(float distance);
+	void Rotate(float angle);
+
+};
+
+#endif
